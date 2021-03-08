2c15b2df5041035acb6c243d2aebe2f22a1ae5c9
wabdsgn <wab2com@gmail.com>
2021-03-08 23:26:35 +0100

    remove staging

--- dashboardConfig.js
+++ dashboardConfig.js
@@ -17,12 +17,12 @@ export default {
             buildHookId: '5f9d8c12b1c666b6f77927a1',
             name: 'suggestion',
           },
-          {
-            title: 'Staging',
-            apiId: 'd090f3a9-2767-41b8-8dc1-6e72370b6316',
-            buildHookId: '5f43d2687dc19c67dc44420f',
-            name: 'suggestion',
-          },
+          // {
+          //   title: 'Staging',
+          //   apiId: 'd090f3a9-2767-41b8-8dc1-6e72370b6316',
+          //   buildHookId: '5f43d2687dc19c67dc44420f',
+          //   name: 'suggestion',
+          // },
         ],
       },
     },
